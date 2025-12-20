74e4e165a309b0d271aacb418a61cc87ddd10b10
Tomer Iwan <iwantomer@hotmail.com>
2025-12-20 22:39:00 +0100

    feat: screenshots to clipboard

--- i3/.config/i3/scripts/screenshot.sh
+++ i3/.config/i3/scripts/screenshot.sh
@@ -5,9 +5,15 @@ FILE_PATH="/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ').png"
 
 main() {
     case $1 in
-        full) maim --format=png $FILE_PATH ;;
-        select) maim --select $FILE_PATH ;;
-        window) maim --window $(xdotool getactivewindow) $FILE_PATH
+        full)
+            maim --format=png "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            ;;
+        select)
+            maim --select "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            ;;
+        window)
+            maim --window $(xdotool getactivewindow) "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            ;;
     esac
 }
 
