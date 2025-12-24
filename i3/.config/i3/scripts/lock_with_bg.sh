2ac07d075d5793798b62eeae0d85486b49fde883
Tomer Iwan <iwantomer@hotmail.com>
2025-12-24 22:31:34 +0100

    feat: blur lockscreen

--- i3/.config/i3/scripts/lock_with_bg.sh
+++ i3/.config/i3/scripts/lock_with_bg.sh
@@ -1,5 +1,7 @@
+
 #!/bin/bash
 
+# Requires: i3lock, imagemagick
 # Set paths
 BG_IMAGE="$HOME/.local/share/backgrounds/bg.png"
 TMP_BG="/tmp/i3lock_bg.png"
@@ -7,10 +9,11 @@ TMP_BG="/tmp/i3lock_bg.png"
 # Get screen resolution (works for single monitor setups)
 RES=$(xdpyinfo | awk '/dimensions/{print $2}')
 
-# Scale the image to fit the screen
-convert "$BG_IMAGE" -resize "$RES^" -gravity south -extent "$RES" "$TMP_BG"
+# Blur and dim the image
+convert "$BG_IMAGE" -resize "$RES^" -gravity center -extent "$RES" \
+	-blur 0x8 -fill black -colorize 40% "$TMP_BG"
 
-# Call i3lock with the scaled image
+# Call i3lock with the processed image
 i3lock -i "$TMP_BG"
 
 # Optionally, remove the temp file after unlocking
