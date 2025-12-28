5385a0681034f83d7fea57c77bb64a77da796c3a
Tomer Iwan <iwantomer@hotmail.com>
2025-12-28 11:10:11 +0100

    change: remove transparency lockscreen

--- i3/.config/i3/scripts/lock_with_bg.sh
+++ i3/.config/i3/scripts/lock_with_bg.sh
@@ -9,9 +9,9 @@ TMP_BG="/tmp/i3lock_bg.png"
 # Get screen resolution (works for single monitor setups)
 RES=$(xdpyinfo | awk '/dimensions/{print $2}')
 
-# Blur and dim the image
+# Blur the image only (no dimming or transparency)
 convert "$BG_IMAGE" -resize "$RES^" -gravity center -extent "$RES" \
-	-blur 0x8 -fill black -colorize 40% "$TMP_BG"
+	-blur 0x8 "$TMP_BG"
 
 # Call i3lock with the processed image
 i3lock -i "$TMP_BG"
