f750d6b8dfda66cd5ad4beaccb24807409e1fe64
Tomer Iwan <iwantomer@hotmail.com>
2026-01-21 17:01:51 +0100

    change: use flameshot for screenshots

--- i3/.config/i3/scripts/screenshot.sh
+++ i3/.config/i3/scripts/screenshot.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+
 # Use the current timestamp as the unique filename of the screenshot.
 FILE_PATH="/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ').png"
 
 main() {
     case $1 in
         full)
-            maim --format=png "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            # Fullscreen capture, save to file and copy to clipboard
+            flameshot full -c -p "$FILE_PATH"
             ;;
         select)
-            maim --select "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            # GUI region select, save to file and copy to clipboard
+            flameshot gui -c -p "$FILE_PATH"
             ;;
         window)
-            maim --window $(xdotool getactivewindow) "$FILE_PATH" && xclip -selection clipboard -t image/png -i "$FILE_PATH"
+            # Simulate window capture by letting user select a window region
+            flameshot gui -c -p "$FILE_PATH"
             ;;
     esac
 }
